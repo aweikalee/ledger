6806fd777bd42a21b4c36c1caed131f55f0df52a
aweikalee <aweikalee@163.com>
2019-10-02 15:56:59 +0800

    feat(Component): Input.Label 增加属性 description

--- src/components/Input/Label.tsx
+++ src/components/Input/Label.tsx
@@ -3,13 +3,16 @@ import clsx from 'clsx'
 import styles from './Input.module.scss'
 
 export interface IInputLabelProps
-    extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+    extends React.LabelHTMLAttributes<HTMLLabelElement> {
+    description?: React.ReactNode
+}
 
 const InputLabel = React.forwardRef<HTMLLabelElement, IInputLabelProps>(
     (props, ref) => {
         const {
             className: classNameProp,
             children,
+            description,
             ...other
         }: typeof props = props
 
@@ -21,9 +24,14 @@ const InputLabel = React.forwardRef<HTMLLabelElement, IInputLabelProps>(
 
         return (
             <div data-role="input-label-box" className={className}>
-                <label data-role="input-label" {...bindProps}>
-                    {children}
-                </label>
+                <div data-role="input-label-wrapper">
+                    <label data-role="input-label" {...bindProps}>
+                        {children}
+                    </label>
+                </div>
+                {description && (
+                    <div data-role="input-label-description">{description}</div>
+                )}
             </div>
         )
     }
