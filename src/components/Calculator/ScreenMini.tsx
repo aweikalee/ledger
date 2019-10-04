5493865e975e83e9d711aa7e8c309ea0361b52e1
aweikalee <aweikalee@163.com>
2019-10-04 10:13:17 +0800

    refactor(components): Calculator/ScreenMini.tsx 改成实时重算文字缩放

--- src/components/Calculator/ScreenMini.tsx
+++ src/components/Calculator/ScreenMini.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useRef, useEffect, useState, useCallback } from 'react'
 import clsx from 'clsx'
 import BigNumberOrigin from 'bignumber.js'
 import measure from '../utils/measure'
@@ -6,6 +6,7 @@ import * as amount from '@/utils/amount'
 import styles from './Screen.module.scss'
 import styleVars from '@/style/index.module.scss'
 import { FORMAT_OPTIONS } from './config'
+import useResizeObserver from '../utils/useResizeObserver'
 
 const BigNumber = BigNumberOrigin.clone({ EXPONENTIAL_AT: 1e9 })
 
@@ -14,40 +15,64 @@ export interface IKeyboradScreenProps
     value?: string
 }
 
-const Screen: React.FC<IKeyboradScreenProps> = props => {
-    const {
-        className: classNameProp,
-        children: childrenProp,
-        value = '0',
-        ...other
-    }: typeof props = props
+const Component = React.forwardRef<HTMLDivElement, IKeyboradScreenProps>(
+    (props, ref) => {
+        const {
+            className: classNameProp,
+            children: childrenProp,
+            value = '0',
+            ...other
+        }: typeof props = props
 
-    const [screen, setScreen] = useState<string>('0.00')
-    useEffect(() => {
-        setScreen(getScreenValue(value))
-    }, [value])
+        const el = useRef<HTMLDivElement>(null)
+        React.useImperativeHandle(ref, () => el.current!)
 
-    const [textScale, setTextScale] = useState(1)
-    useEffect(() => {
-        setTextScale(getTextScale(screen))
-    }, [screen])
+        const [screen, setScreen] = useState<string>('0.00')
+        useEffect(() => {
+            setScreen(getScreenValue(value))
+        }, [value])
 
-    const className = clsx(styles['screen-mini'], classNameProp)
-    const bindProps = {
-        className,
-        ...other
+        const [textScale, setTextScale] = useState(1)
+
+        const resizeHandler = useCallback(() => {
+            if (!el.current) {
+                return
+            }
+
+            const base = el.current.offsetWidth
+            const { fontSize } = getComputedStyle(el.current)
+            const width = measure(screen, {
+                fontFamily: styleVars.fontFamily,
+                fontSize: fontSize || '0.68rem'
+            }).width
+
+            setTextScale(width < base ? 1 : (base / width) * 0.95)
+        }, [screen])
+
+        useEffect(() => {
+            resizeHandler()
+        }, [resizeHandler])
+
+        useResizeObserver(el, resizeHandler)
+
+        const className = clsx(styles['screen-mini'], classNameProp)
+        const bindProps = {
+            className,
+            ...other
+        }
+
+        return (
+            <div data-role="calculator-screen-mini" ref={el} {...bindProps}>
+                <span data-role="mini" style={{ fontSize: `${textScale}em` }}>
+                    {screen}
+                </span>
+            </div>
+        )
     }
+)
 
-    return (
-        <div data-role="calculator-screen-mini" {...bindProps}>
-            <span data-role="mini" style={{ fontSize: `${textScale}em` }}>
-                {screen}
-            </span>
-        </div>
-    )
-}
-
-export default Screen
+Component.displayName = 'CalculatorScreenMini'
+export default Component
 
 function getScreenValue(value: string) {
     const bignumber = new BigNumber(value)
@@ -63,18 +88,4 @@ function getScreenValue(value: string) {
         result = amount.format(bignumber.toFixed(2), FORMAT_OPTIONS)
     }
     return result
-}
-
-let baseTextWidth: number
-function getTextScale(text: string) {
-    if (!baseTextWidth) {
-        baseTextWidth = measure('1234567890123456', {
-            fontFamily: styleVars.fontFamily
-        }).width
-    }
-    const width = measure(text, {
-        fontFamily: styleVars.fontFamily
-    }).width
-
-    return width < baseTextWidth ? 1 : baseTextWidth / width
 }
